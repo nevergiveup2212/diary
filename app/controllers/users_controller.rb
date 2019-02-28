380914c49ce98e493e97b9b118057f44dab5a936
HienHM <blackcat22121996@gmail.com>
2019-03-01 01:03:21 +0700

    Create Diary website

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+  def index
+    
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      flash[:success] = "Register success"
+      redirect to users_path
+    else
+      flash[:success] = "Register failed"
+      render :new
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit :name, :password, :password_confirmation
+  end
+  
+end
